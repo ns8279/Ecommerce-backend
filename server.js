94c367bfd6c70b06c5ea8d9c9324256f911355a6
Nirupama Sundararaman <nirupama.2308@gmail.com>
2020-06-05 06:41:26 -0700

    establishing a connection

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
+const sequelize = require('./config/connection.js');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -11,6 +12,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+// app.listen(PORT, () => {
+//   console.log(`App listening on port ${PORT}!`);
+// });
+sequelize.sync({ force: false }).then(() => { //once you have the association in index.js file , change the force = true and start the server to drop and create the tables. Once thats done, change it back to false
+  app.listen(PORT, () => console.log('Now listening'));
 });
